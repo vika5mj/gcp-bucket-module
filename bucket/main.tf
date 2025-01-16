7a14af289eada634e21f15e1b95297102b9d359f
vika5mj <37324318+vika5mj@users.noreply.github.com>
2025-01-16 13:23:43 +0530

    Update main.tf

--- bucket/main.tf
+++ bucket/main.tf
@@ -1,7 +1,7 @@
 module "gcs_buckets" {
   source  = "terraform-google-modules/cloud-storage/google"
   version = "~> 8.0"
-  names = var.name
+  names = var.names
   project_id = var.project_id
   prefix = var.prefix
 }
